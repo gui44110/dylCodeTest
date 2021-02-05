4e4958666f6e1315a9557d96f34f857ec81a8b88
dongyuliang <dongyuliang@meituan.com>
2021-02-05 16:20:11 +0800

    problem20

--- src/test/java/com/dyl/code/leetcode/top100/between20/Problem19.java
+++ src/test/java/com/dyl/code/leetcode/top100/between20/Problem19.java
@@ -31,6 +31,12 @@ public class Problem19 {
 
 
     public int removeDuplicates(int[] nums) {
-
+        int result = 0;
+        for(int i = 1 ;i < nums.length;i++){
+            if(nums[i] != nums[result]){
+                result ++;
+            }
+        }
+        return result + 1;
     }
 }
